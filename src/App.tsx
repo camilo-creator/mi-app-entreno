bc3fe783c1f71f81401aade56b91aa09f0274df1
camilo-creator <68717253+camilo-creator@users.noreply.github.com>
2026-04-10 15:16:20 -0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{padding: 20}}>
+      <h1>App Calistenia 🚀</h1>
+      <p>Tu base está lista. Ahora reemplaza este App.tsx por el completo.</p>
+    </div>
+  );
+}
